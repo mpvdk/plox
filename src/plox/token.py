6c5f2e2ffe4f1dea6326093a60152a3f08360d12
martijn <martijnvdkerkhof@gmail.com>
2023-12-21 16:30:40 +0100

    update token.py

--- src/plox/token.py
+++ src/plox/token.py
@@ -1,5 +1,5 @@
-from typing import Any
 from plox.token_type import TokenType
+from typing import Any
 
 class Token:
     """
@@ -16,4 +16,4 @@ class Token:
         self.line = line
 
     def to_string(self) -> str:
-        return f"{self.token_type} {self.lexeme} {self.literal}"
+        return f"Type:    {self.token_type}\nLexeme:  {self.lexeme}\nLiteral: {self.literal}"
